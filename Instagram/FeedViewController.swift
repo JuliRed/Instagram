b651fe62dcb8d1e349aa8f5a0a16002b97af2221
JuliRed <julianaredondo15@gmail.com>
2021-03-18 20:49:24 -0400

    Comments box
    Changes in login and logout

--- Instagram/FeedViewController.swift
+++ Instagram/FeedViewController.swift
@@ -68,6 +68,25 @@ class FeedViewController: UIViewController, UITableViewDataSource, UITableViewDe
         
             return cell
     }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let post = posts[indexPath.row]
+        
+        let comment = PFObject(className: "Comments")
+        comment["text"] = "This is a random comment"
+        comment["post"] = post
+        comment["author"] = PFUser.current()!
+        
+        post.add(comment, forKey: "comments")
+        
+        post.saveInBackground { (success, error) in
+            if success {
+                print("Comment saved")
+            } else {
+                print("Error saving comment")
+            }
+        }
+    }
 
         
 
@@ -80,6 +99,25 @@ class FeedViewController: UIViewController, UITableViewDataSource, UITableViewDe
         // Pass the selected object to the new view controller.
     }
     */
+    
+    @IBAction func onLogoutButton(_ sender: Any) {
+        PFUser.logOut()
+        
+        let main = UIStoryboard(name: "Main", bundle: nil)
+        let loginViewCOntroller = main.instantiateViewController(withIdentifier: "LoginViewController")
+        
+        let delegate = UIApplication.shared.delegate as! SceneDelegate
+        
+        delegate.window?.rootViewController = loginViewCOntroller
+        
+        
+        
+        
+    }
+    
+    
+    
+    
 }
 
 
